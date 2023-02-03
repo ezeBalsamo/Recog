f10885e84bf31af93bac294d93e3bab61aee5303
FedericoFuentesWeber <fedefuentes2112@gmail.com>
2023-02-03 06:33:32 -0300

    :fire: Remove unused local-auth.module-definition.ts file

--- src/local/local-auth.module-definition.ts
+++
@@ -1,5 +0,0 @@
-import { ConfigurableModuleBuilder } from '@nestjs/common'
-import { LocalAuthModuleOptions } from './local-auth.interface'
-
-export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
-  new ConfigurableModuleBuilder<LocalAuthModuleOptions>().build()
