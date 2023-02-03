caeeac326502518cb097e68449ab54c0e9665278
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-02-03 05:24:51 -0300

    :passport_control: Use guards in google auth controller routes
    Signed-off-by: ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>

--- src/google/google-auth.controller.ts
+++ src/google/google-auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Inject, Request } from '@nestjs/common'
+import { Controller, Inject, Request, UseGuards } from '@nestjs/common'
 import {
   GoogleAuthModuleOptions,
   GoogleAuthServiceBehaviour,
 } from './google-auth.interface'
 import { MODULE_OPTIONS_TOKEN } from './google-auth.module-definition'
+import { GoogleAuthGuard } from './google-auth.guard'
 
 @Controller()
 export class GoogleAuthController {
@@ -16,9 +17,11 @@ export class GoogleAuthController {
     this.authService = options.authService
   }
 
+  @UseGuards(GoogleAuthGuard)
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   async login() {}
 
+  @UseGuards(GoogleAuthGuard)
   async redirect(@Request() req) {
     return this.authService.handleRedirectionFor(req.user)
   }
