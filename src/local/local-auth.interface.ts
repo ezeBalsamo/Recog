90a698baefe4153fd069fd52f66956b2e672fab4
Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>
2023-02-06 17:55:55 -0300

    :construction: Add LocalAuthServiceBehaviour
    Signed-off-by: FedericoFuentesWeber <fedefuentes2112@gmail.com>

--- src/local/local-auth.interface.ts
+++ src/local/local-auth.interface.ts
@@ -1,6 +1,12 @@
 import { IStrategyOptions, IStrategyOptionsWithRequest } from 'passport-local'
 
+export interface LocalAuthServiceBehaviour {
+  validatedUserFrom: (username: string, password: string) => Promise<unknown>
+
+  handleLoginFor(user: any): Promise<any>
+}
+
 export interface LocalAuthModuleOptions {
   strategyOptions?: IStrategyOptions | IStrategyOptionsWithRequest
-  handleLoginFor: (username: string, password: string) => Promise<unknown>
+  authService: LocalAuthServiceBehaviour
 }
