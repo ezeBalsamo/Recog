fbc4bb97997de791af908da76288cbfd2f8f5917
Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>
2023-02-07 19:15:32 -0300

    :white_check_mark: Add test: should throw unauthorized error when username and password fields are undefined
    Signed-off-by: Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>

--- test/local-auth-module.e2e-spec.ts
+++ test/local-auth-module.e2e-spec.ts
@@ -20,6 +20,18 @@ describe('LocalAuthModule (e2e)', () => {
   })
 
   describe('/login (POST)', () => {
+    it('should throw an Unauthorized (HTTP 401) error when username and password fields are undefined', () => {
+      return request(app.getHttpServer())
+        .post('/login')
+        .send({})
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(401)
+        .expect({
+          statusCode: 401,
+          message: 'Unauthorized',
+        })
+    })
+
     it('should throw an Unauthorized (HTTP 401) error when password field is absent', () => {
       return request(app.getHttpServer())
         .post('/login')
