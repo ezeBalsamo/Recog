13b18bb894df64b7071797ba6b8635cb78d5c955
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-02-16 02:52:30 -0300

    :construction: Import service definition in module decorator
    Signed-off-by: ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>

--- src/google/google-auth.module.ts
+++ src/google/google-auth.module.ts
@@ -3,10 +3,22 @@ import { ConfigurableModuleClass } from './google-auth.module-definition'
 import { PassportModule } from '@nestjs/passport'
 import { GoogleAuthStrategy } from './google-auth.strategy'
 import { GoogleAuthController } from './google-auth.controller'
+import config from 'recog-config'
+import { GOOGLE_AUTH_SERVICE_CLASS } from './google-auth.constants'
+
+const {
+  google: { authServiceDefinition },
+} = config
 
 @Module({
-  imports: [PassportModule],
-  providers: [GoogleAuthStrategy],
+  imports: [PassportModule, ...(authServiceDefinition.imports || [])],
+  providers: [
+    GoogleAuthStrategy,
+    {
+      provide: GOOGLE_AUTH_SERVICE_CLASS,
+      useClass: authServiceDefinition.class,
+    },
+  ],
   controllers: [GoogleAuthController],
 })
 export class GoogleAuthModule extends ConfigurableModuleClass {}
