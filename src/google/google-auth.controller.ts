bb44a48d870a60b44264999897aaba480403f5ee
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-02-16 02:55:01 -0300

    :construction: Add routes to controller
    Signed-off-by: ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>

--- src/google/google-auth.controller.ts
+++ src/google/google-auth.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Inject, Request, UseGuards } from '@nestjs/common'
+import { Controller, Get, Inject, Request, UseGuards } from '@nestjs/common'
 import { GoogleAuthServiceBehaviour } from './google-auth.interface'
 import { GoogleAuthGuard } from './google-auth.guard'
 import { GOOGLE_AUTH_SERVICE_CLASS } from './google-auth.constants'
+import config from 'recog-config'
 
-@Controller()
+const {
+  google: { routes },
+} = config
+
+@Controller(routes.prefix)
 export class GoogleAuthController {
   constructor(
     @Inject(GOOGLE_AUTH_SERVICE_CLASS)
@@ -11,10 +16,12 @@ export class GoogleAuthController {
   ) {}
 
   @UseGuards(GoogleAuthGuard)
+  @Get(routes.loginPath)
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   async login() {}
 
   @UseGuards(GoogleAuthGuard)
+  @Get(routes.redirectPath)
   async redirect(@Request() req) {
     return this.authService.handleRedirectionFor(req.user)
   }
