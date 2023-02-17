df6bd49ac3d517ee14fb676d3051cec4afa11e85
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-02-16 23:00:58 -0300

    :coffin: Remove auth module definition
    Signed-off-by: ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>

--- src/google/google-auth.module-definition.ts
+++
@@ -1,5 +0,0 @@
-import { ConfigurableModuleBuilder } from '@nestjs/common'
-import { GoogleAuthStrategyOptions } from './google-auth.interface'
-
-export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
-  new ConfigurableModuleBuilder<GoogleAuthStrategyOptions>().build()
