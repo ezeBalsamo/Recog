2172f312c1821d73fa3f291a43cf5247ace0bf99
Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>
2023-02-25 00:14:48 -0300

    :white_check_mark: Add test: can be registered with implicit auth service definition imports
    Signed-off-by: Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>

---
+++ src/github/specs/github-auth.module.spec.ts
@@ -0,0 +1,31 @@
+import { MockGithubAuthService } from './mock.github-auth.service'
+import { GithubAuthServiceDefinition } from '../github-auth.interface'
+import { GithubAuthModule } from '../github-auth.module'
+import { Test } from '@nestjs/testing'
+
+const assertCanInitialiseWhenAuthServiceDefinedBy = async (
+  authServiceDefinition: GithubAuthServiceDefinition,
+) => {
+  const testingModule = await Test.createTestingModule({
+    imports: [
+      GithubAuthModule.register({
+        strategyOptions: {
+          clientID: 'pennyworth1',
+          clientSecret: 'Bruce Wayne is Batman',
+          callbackURL: 'http://localhost:3000/auth/github/callback',
+        },
+        authServiceDefinition,
+      }),
+    ],
+  }).compile()
+  const app = testingModule.createNestApplication()
+  await app.init()
+}
+
+describe('GithubAuthModule', () => {
+  it('can be registered with implicit auth service definition imports', async () => {
+    await assertCanInitialiseWhenAuthServiceDefinedBy({
+      class: MockGithubAuthService,
+    })
+  })
+})
