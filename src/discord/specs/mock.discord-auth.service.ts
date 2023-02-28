b8aca212385fa488b9e99e4a68eadeada52b4d52
Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>
2023-02-28 00:50:56 -0300

    :white_check_mark: Add test: should validate provided credentials
    Signed-off-by: Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>

---
+++ src/discord/specs/mock.discord-auth.service.ts
@@ -0,0 +1,30 @@
+import { DiscordAuthServiceBehaviour } from '../discord-auth.interface'
+
+export class MockDiscordAuthService implements DiscordAuthServiceBehaviour {
+  handleLoginFor(
+    accessToken: string,
+    refreshToken: string,
+    profile: {
+      displayName: string
+      flags: number
+      verified: boolean
+      banner: string
+      fetchedAt: string
+      avatar: string
+      locale: string
+      discriminator: string
+      accent_color: number
+      provider: string
+      mfa_enabled: boolean
+      id: string
+      email: string
+      username: string
+    },
+  ): Promise<any> {
+    return Promise.resolve({
+      username: profile.username,
+      id: profile.id,
+      superhero: true,
+    })
+  }
+}
