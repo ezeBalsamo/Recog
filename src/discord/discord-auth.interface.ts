29f6a839abdf0d4084edd83091c611ec79ad070f
Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>
2023-02-28 00:45:05 -0300

    :recycle: Specify profile fields
    Signed-off-by: Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>

--- src/discord/discord-auth.interface.ts
+++ src/discord/discord-auth.interface.ts
@@ -1,11 +1,25 @@
-import { Profile } from 'passport-github2'
 import { StrategyOptions } from 'passport-discord'
 
 export interface DiscordAuthServiceBehaviour {
   handleLoginFor(
     accessToken: string,
     refreshToken: string,
-    profile: Profile,
+    profile: {
+      displayName: string
+      flags: number
+      verified: boolean
+      banner: string
+      fetchedAt: string
+      avatar: string
+      locale: string
+      discriminator: string
+      accent_color: number
+      provider: string
+      mfa_enabled: boolean
+      id: string
+      email: string
+      username: string
+    },
   ): Promise<any>
 }
 
